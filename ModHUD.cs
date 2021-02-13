7af2cf97de08c5e8f4a2ccf99cef442bd37df07f
Immanuel Van Meirhaeghe <immanuelvm@live.be>
2021-02-13 14:23:16 +0100

    tester

--- ModHUD.cs
+++ ModHUD.cs
@@ -24,7 +24,7 @@ namespace ModAudio
         private static readonly float ModScreenMinHeight = 300f;
         private static readonly float ModScreenMaxHeight = 300f;
         private static float ModScreenStartPositionX { get; set; } = 50f;
-        private static float ModScreenStartPositionY { get; set; } = Screen.height - ModScreenTotalHeight- 50f;
+        private static float ModScreenStartPositionY { get; set; } = Screen.height - ModScreenTotalHeight- 75f;
         private static bool IsMinimized { get; set; } = false;
         private bool ShowUI = false;
         public static Rect ModHUDScreen = new Rect(ModScreenStartPositionX, ModScreenStartPositionY, ModScreenTotalWidth, ModScreenTotalHeight);
@@ -372,6 +372,7 @@ namespace ModAudio
                 Color defaultC = GUI.color;
                 GUIStyle labelStyle = new GUIStyle(GUI.skin.label)
                 {
+                    fixedWidth = 25f,
                     fontSize = 18
                 };
 
@@ -389,49 +390,52 @@ namespace ModAudio
                             }
                             Quaternion rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
                             string direction = string.Empty;
-                            if (rotation.z == 0.0f && Math.Abs(rotation.w) == 1.0f)
+                            double z = Math.Round(rotation.z, 1, MidpointRounding.ToEven);
+                            double w = Math.Round(rotation.w, 1, MidpointRounding.ToEven);
+
+                            if (z == 0.0f && Math.Abs(w) == 1.0f)
                             {
                                 GUI.color = defaultC;
                                 direction = "N";
                             }
-                            if (rotation.z > 0.1f && rotation.z <= 0.4f && rotation.w >= -0.9f && rotation.w < -0.7f)
+                            if (z > 0.1f && z <= 0.4f && w >= -0.9f && w < -0.7f)
                             {
                                 GUI.color = defaultC;
                                 direction = "NW";
                             }
-                            if (rotation.z == 0.7f && rotation.w == -0.7f)
+                            if (z== 0.7f && w == -0.7f)
                             {
                                 GUI.color = IconColors.GetColor(IconColors.Icon.Hydration);
                                 direction = "W";
                             }
-                            if (rotation.z > 0.7f && rotation.z < 1.0f  && rotation.w > -0.7f && rotation.w < 0.0f)
+                            if (z> 0.7f && z< 1.0f  && w > -0.7f && w < 0.0f)
                             {
                                 GUI.color = defaultC;
                                 direction = "SW";
                             }
-                            if (Math.Abs(rotation.z) == 1.0f && rotation.w == 0.0f)
+                            if (Math.Abs(Math.Round(rotation.z)) == 1.0f && w == 0.0f)
                             {
                                 GUI.color = IconColors.GetColor(IconColors.Icon.Proteins);
                                 direction = "S";
                             }
-                            if (rotation.z > 0.7f && rotation.w > 0.0f && rotation.w < 0.7f)
+                            if (z> 0.7f && w > 0.0f && w < 0.7f)
                             {
                                 GUI.color = defaultC;
                                 direction = "SE";
                             }
-                            if (rotation.z == 0.7f && rotation.w == 0.7f)
+                            if (z== 0.7f && w == 0.7f)
                             {
                                 GUI.color = defaultC;
                                 direction = "E";
                             }
-                            if (rotation.z > 0.3f && rotation.w >= 0.8f && rotation.w < 1.0f)
+                            if (z> 0.3f && w >= 0.8f && w < 1.0f)
                             {
                                 GUI.color = defaultC;
                                 direction = "NE";
                             }
-                            GUILayout.Label($"{rotation.z}", GUI.skin.label);
+                            GUILayout.Label($"{z}", GUI.skin.label, GUILayout.Width(25f));
                             GUILayout.Label($"{direction}", labelStyle);
-                            GUILayout.Label($"{rotation.w}", GUI.skin.label);
+                            GUILayout.Label($"{w}", GUI.skin.label,GUILayout.Width(25f));
                         }
 
                         using (var positionScope = new GUILayout.VerticalScope(GUI.skin.label))
@@ -444,25 +448,24 @@ namespace ModAudio
                                 GUI.color = defaultC;
                                 GUILayout.Label($"{ GPSCoordinatesW}", labelStyle);
                                 GUI.color = IconColors.GetColor(IconColors.Icon.Hydration);
-                                GUILayout.Label($"\"W", labelStyle);
+                                GUILayout.Label($"\'W", labelStyle);
                             }
                             using (var coordinatesSScope = new GUILayout.HorizontalScope(GUI.skin.label))
                             {
                                 GUI.color = defaultC;
                                 GUILayout.Label($"{ GPSCoordinatesS}", labelStyle);
                                 GUI.color = IconColors.GetColor(IconColors.Icon.Proteins);
-                                GUILayout.Label($"\"S", labelStyle);
+                                GUILayout.Label($"\'S", labelStyle);
                             }
                         }
                     }
                 }
                 else
                 {
-                    using (var infoScope = new GUILayout.VerticalScope(GUI.skin.box))
+                    using (var infoScope = new GUILayout.VerticalScope(GUI.skin.label))
                     {
                         GUI.color = Color.yellow;
                         GUILayout.Label(OnlyForSinglePlayerOrHostMessage(), GUI.skin.label);
-                        GUI.color = Color.white;
                     }
                 }
                 GUI.color = defaultC;
